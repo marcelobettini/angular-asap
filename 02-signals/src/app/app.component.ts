f91518030428c67528ac3049ee6b847b1272d8af
Marcelo Bettini <bettinimarcelo@gmail.com>
2025-09-08 11:10:27 -0300

    descripcion mostrar mas o menos

--- 02-signals/src/app/app.component.ts
+++ 02-signals/src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core'
+import { Component, signal, computed, WritableSignal, Signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { ProductListComponent } from "./product-list/product-list.component"
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ProductListComponent, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Signals';
+  oldCount = 0;
+  showCount: WritableSignal<boolean> = signal(false)
+  count: WritableSignal<number> = signal(0)
+  doubleCount: Signal<number> = computed(() => this.count() * 2)
+  conditionalMessage = computed(() => {
+    if (this.showCount()) {
+      return `The conditional message is printed. ${this.count()}.`
+    } else {
+      return `Nothing to see here... yet`
+    }
+  })
+
+  //computed values with Signals
+
+  addOneToOldCount() {
+    this.oldCount++
+  }
+
+  addOne() {
+    this.count.update(prev => prev + 1)
+  }
+  subOne() {
+    this.count.update(prev => prev - 1)
+  }
+  reset() {
+    this.count.set(0)
+  }
+
+  toggleShowcount() {
+    this.showCount.update(prev => !prev)
+
+  }
 }
+
---
+++ 02-signals/src/app/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, signal, WritableSignal } from '@angular/core'
+import products from '../../data/products.json'
+import { Product } from '../../data/product.type'
+import { CommonModule } from '@angular/common'
+import { ProductCardComponent } from "../product-card/product-card.component"
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule, ProductCardComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  products = signal<Product[]>(products)
+  addProduct(newProduct: Product) {
+    this.products.update((current) => [...current, newProduct])
+  }
+}
