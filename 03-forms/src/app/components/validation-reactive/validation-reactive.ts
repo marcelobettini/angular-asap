2f9171b769e084c10104bbf25f7ff47ca635536a
Marcelo Bettini <bettinimarcelo@gmail.com>
2025-09-17 11:16:01 -0300

    validation reactive forms, debugging

--- 03-forms/src/app/components/validation-reactive/validation-reactive.ts
+++ 03-forms/src/app/components/validation-reactive/validation-reactive.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-validation-reactive',
-  imports: [],
+  imports: [ReactiveFormsModule,],
+  standalone: true,
   templateUrl: './validation-reactive.html',
   styleUrl: './validation-reactive.css'
 })
 export class ValidationReactive {
+  user = {
+    name: "",
+    email: ""
+  }
+  userForm = new FormGroup({
+    name: new FormControl(this.user.name, [
+      Validators.required,
+      Validators.minLength(5)
+    ]),
+    email: new FormControl(this.user.email, [
+      Validators.required,
+      Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+    ])
+  })
+
+  get nameControl() {
+    return this.userForm.get("name")!
+  }
+  get emailControl() {
+    return this.userForm.get("email")!
+  }
+
+  handleSubmit($event: Event) {
+    $event.preventDefault()
+    if (this.userForm.valid) {
+      this.user.name = this.userForm.value.name || ''
+      this.user.email = this.userForm.value.email || ''
+      alert(`USER NAME: ${this.user.name} - EMAIL: ${this.user.email}`)
+    } else {
+      this.userForm.markAllAsTouched()
+      alert("Please fix errors")
+    }
+
+  }
 
 }
